334d726a1b10127c48b8c993af1defd7e2dda505
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2019-08-29 22:26:20 -0500

    initial commit

---
+++ src/main/java/test101/basic/programming/Test101PrimeNumber.java
@@ -0,0 +1,17 @@
+package test101.basic.programming;
+
+import java.util.Scanner;
+
+public class Test101PrimeNumber {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int userInput = SCANNER.nextInt();
+
+        for (int index = 1; index <= userInput; index++) {
+
+        }
+    }
+}
