1f7facec057b2f4cf2161189d85edecb2ed2274e
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2019-09-02 21:28:40 -0500

    added pattern implementation

---
+++ src/main/java/test101/basic/programming/Test105PatternWithChisel.java
@@ -0,0 +1,61 @@
+package test101.basic.programming;
+
+import java.util.Scanner;
+
+/**
+ * For a given number crate following pattern which should scale depending on the number.
+ *
+ * Input : 5
+ *
+ * Output:
+ *          *########*
+ *          **######**
+ *          ***####***
+ *          ****##****
+ *          **********
+ */
+public class Test105PatternWithChisel {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int givenNumber = SCANNER.nextInt();
+
+        createPattern(givenNumber);
+
+    }
+
+    /**
+     *          *################*
+     *          **##############**
+     *          ***############***
+     *          ****##########****
+     *          *****########*****
+     *          ******######******
+     *          *******####*******
+     *          ********##********
+     *          ******************
+     *
+     * @param givenNumber
+     */
+    private static void createPattern(int givenNumber) {
+
+        int verticalLimit = givenNumber;
+        int horizontalLimit = givenNumber * 2;
+
+
+        for (int verticalIndex = 1; verticalIndex <= verticalLimit; verticalIndex++) {
+
+            for (int horizontalIndex = 1; horizontalIndex <= horizontalLimit; horizontalIndex++) {
+
+                if (horizontalIndex > verticalIndex && (horizontalIndex <= (horizontalLimit - verticalIndex))) {
+                    System.out.print("#");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
