0a04f5f4c78bca102ca5a3ee34b9bb1051424183
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2019-09-21 22:14:54 -0500

    added counting numbers

---
+++ src/main/java/test101/basic/programming/Test115CountingNumbers.java
@@ -0,0 +1,47 @@
+package test101.basic.programming;
+
+import java.util.Scanner;
+
+/**
+ * For a given alphanumeric string (text), Find out how many numbers are in that string (text)
+ * Note: we are checking for numbers, not digit.
+ *
+ * Input :   mike123hello1212eee1
+ * Output:   3
+ *
+ * @see <a href="https://github.com/pankajnimgade/PracticalProgramming/blob/master/src/main/java/test101/basic/programming/Test114DiceSequence.java">Dice Sequence</a>
+ */
+public class Test115CountingNumbers {
+
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        String givenAlphaNumericNumber = SCANNER.next();
+
+        int countNumber = 0;
+        boolean foundDigit = false;
+        // mike123hello1212eee1
+        for (char currentCharacter : givenAlphaNumericNumber.toCharArray()) {
+
+            if (isDigit(currentCharacter)) {
+                foundDigit = true;
+            } else {
+                if (foundDigit) {
+                    foundDigit = false;
+                    countNumber++;
+                }
+            }
+        }
+
+        if (foundDigit) {
+            countNumber++;
+        }
+
+        System.out.println(countNumber);
+    }
+
+    private static boolean isDigit(char currentCharacter) {
+        return Character.isDigit(currentCharacter);
+    }
+}
